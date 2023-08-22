baedf10f3a310cb7e14ed8e1e9d289b289e65535
Mohammad Alkandari <Mohammad.Alkandari@WARBABANK.COM>
2023-08-22 13:11:45 +0300

    added custom callout view

---
+++ PACKT-LetsEat/Map Tab/Custom Callout View/CustomCalloutView.swift
@@ -0,0 +1,20 @@
+//
+//  CustomCalloutView.swift
+//  PACKT-LetsEat
+//
+//  Created by Warba on 22/08/2023.
+//
+
+import UIKit
+
+class CustomCalloutView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
